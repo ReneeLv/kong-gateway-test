b07858492a936f84b3c65062683ae1f8374cc661
renee.lv <renee.lv@outlook.com>
2025-08-27 17:30:45 +0800

    fixing test case1

--- cypress/e2e/service-creation.spec.ts
+++ cypress/e2e/service-creation.spec.ts
@@ -3,14 +3,11 @@ import ServicesPage from "../pages/services-page";
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:8002/workspaces');
-  })
-
-  it('service', function() {
 
     cy.get('.workspace-name').click();
     cy.get('[data-testid="sidebar-item-gateway-services"] > .sidebar-item-link > .sidebar-item-display > .sidebar-item-name-container > .sidebar-item-name').click();
 
     ServicesPage.createService('http://httpbin.konghq.com', 'new-service-1');
 
-  });
+  })
 })
