cf335028a8169971e2e311f0e46e0cda656b22b0
renee.lv <renee.lv@outlook.com>
2025-08-27 16:48:23 +0800

    fixing failed test cases

--- cypress/e2e/service-creation.spec.ts
+++ cypress/e2e/service-creation.spec.ts
@@ -5,21 +5,10 @@ describe('template spec', () => {
     cy.visit('http://localhost:8002/workspaces');
   })
 
-
   it('service', function() {
 
     cy.get('.workspace-name').click();
     cy.get('[data-testid="sidebar-item-gateway-services"] > .sidebar-item-link > .sidebar-item-display > .sidebar-item-name-container > .sidebar-item-name').click();
-
-    cy.get('body').then(($body) => {
-      if ($body.find('[data-testid="empty-state-action"]').length) {
-        cy.get('[data-testid="empty-state-action"]').click();
-      } else if ($body.find('[data-testid="toolbar-add-gateway-service"]').length) {
-        cy.get('[data-testid="toolbar-add-gateway-service"]').click();
-      } else {
-        throw new Error('No button found');
-      }
-    });
 
     ServicesPage.createService('http://httpbin.konghq.com', 'new-service-1');
 
--- cypress/pages/services-page.ts
+++ cypress/pages/services-page.ts
@@ -1,11 +1,26 @@
 class ServicesPage {
   elements = {
+    // for handling different createButtion element type
+    createButton: () => {
+      return cy.get('body').then(($body) => {
+        const button1 = $body.find('[data-testid="toolbar-add-gateway-service"]');
+        const button2 = $body.find('[data-testid="empty-state-action"]');
+        
+        if (button1.length) {
+          return cy.wrap(button1);
+        } else if (button2.length) {
+          return cy.wrap(button2);
+        }
+      });
+    },
+
     urlInput: () => cy.get('[data-testid="gateway-service-url-input"]'),
     nameInput: () => cy.get('[data-testid="gateway-service-name-input"]'),
     saveButton: () => cy.get('[data-testid="service-create-form-submit"]')
   }
 
   createService(url: string, name: string) {
+    this.elements.createButton().click();
     this.elements.urlInput().clear();
     this.elements.urlInput().type(url);
     this.elements.nameInput().clear();
