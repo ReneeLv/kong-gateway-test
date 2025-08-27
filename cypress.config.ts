fde1d00bbcac14ac7399ad1945ec0c6dc6803472
renee.lv <renee.lv@outlook.com>
2025-08-27 14:39:52 +0800

    fix compile error

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:8002",
     supportFile: "cypress/support/e2e.ts",
-    specPattern: "cypress/e2e/**/*.spec.ts",
+    // specPattern: "cypress/e2e/**/*.spec.ts",
     viewportWidth: 1280,
     viewportHeight: 720,
     video: true,
